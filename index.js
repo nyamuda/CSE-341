b8a3b73802fd90c2fb452927e9d7b5843f70821b
Pierce Nyamuda <ptnrlab@gmail.com>
2022-09-25 04:59:20 +0200

    cors enabled for all requests

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ let contactRoutes = require("./routes/contacts");
 let models = require("./models/index");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+let cors = require('cors');
 
 let app = express();
 let port = 3000;
 
+
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
 app.use("/", homeRoute);
 app.use("/contacts", contactRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
